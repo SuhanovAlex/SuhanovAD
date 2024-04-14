65234c8813dfb924a76083275142cebb20c3244b
Alexandr Suhanov <delosk@gmail.com>
2024-04-14 22:55:12 +0300

    initial commit

---
+++ HomeWork_2_5_4/HomeWork_2_5_4/HomeWork_2_5_4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <Windows.h>
+#define MODE 1
+
+#if defined MODE
+#if MODE ==1
+double add(double a, double b) {
+    return a + b;
+};
+#endif
+#endif
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+#if defined MODE   
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки\n"; 
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме\n" << "Введите число 1:\t";
+    double a{ 0 };
+    std::cin >> a;
+    std::cout  << "Введите число 2:\t";
+    double b{ 0 };
+    std::cin >> b;
+    std::cout << "Результат сложения:\t" << add(a,b);
+#else
+    std::cout << "Неизвестный режим. Завершение работы\n";
+#endif
+#endif //конец проверки, что MODE определена
+
+#if !defined MODE
+    std::cout << "Необходимо определить MODE.\n";
+#endif//конец проверки, что MODE не определена
+    return 0;
+}
+
