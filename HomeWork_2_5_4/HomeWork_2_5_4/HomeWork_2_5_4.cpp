a14228e247ab38c7ae9275498cc857996a10ad17
Alexandr Suhanov <delosk@gmail.com>
2024-07-23 20:22:47 +0300

    changes after checking

--- HomeWork_2_5_4/HomeWork_2_5_4/HomeWork_2_5_4.cpp
+++ HomeWork_2_5_4/HomeWork_2_5_4/HomeWork_2_5_4.cpp
@@ -2,12 +2,14 @@
 #include <Windows.h>
 #define MODE 1
 
-#if defined MODE
-#if MODE ==1
+#ifndef MODE
+#error Необходимо определить MODE.
+#endif
+
+#if MODE == 1
 double add(double a, double b) {
     return a + b;
 };
-#endif
 #endif
 
 int main()
@@ -16,25 +18,21 @@ int main()
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
 
-#if defined MODE   
 #if MODE == 0
-    std::cout << "Работаю в режиме тренировки\n"; 
+    std::cout << "Работаю в режиме тренировки\n";
+
 #elif MODE == 1
     std::cout << "Работаю в боевом режиме\n" << "Введите число 1:\t";
     double a{ 0 };
     std::cin >> a;
-    std::cout  << "Введите число 2:\t";
+    std::cout << "Введите число 2:\t";
     double b{ 0 };
     std::cin >> b;
-    std::cout << "Результат сложения:\t" << add(a,b);
+    std::cout << "Результат сложения:\t" << add(a, b);
 #else
     std::cout << "Неизвестный режим. Завершение работы\n";
 #endif
-#endif //конец проверки, что MODE определена
 
-#if !defined MODE
-    std::cout << "Необходимо определить MODE.\n";
-#endif//конец проверки, что MODE не определена
     return 0;
 }
 
