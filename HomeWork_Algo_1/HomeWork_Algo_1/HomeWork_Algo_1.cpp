edea3600f9c8c3e52f5c23c6938dfbba67b898e4
Alexandr Suhanov <delosk@gmail.com>
2024-09-09 19:08:49 +0300

    changes after checking

--- HomeWork_Algo_1/HomeWork_Algo_1/HomeWork_Algo_1.cpp
+++ HomeWork_Algo_1/HomeWork_Algo_1/HomeWork_Algo_1.cpp
@@ -1,32 +1,35 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 #include <Windows.h>
 
-int CountNums(int* arr, int num) {
+int CountNums(int* arr, int size, int num) {
     int left = 0;
-    int right = sizeof(arr);
+    int right = size;
+
     while (left < right) {
         int mid = left + (right - left) / 2;
-        if (arr[mid] > num) 
-            right = mid; 
-        
-        else 
-            left = mid + 1; 
-        return sizeof(arr) - left;
-    };
-};
+        if (arr[mid] > num)
+            right = mid;
+        else
+            left = mid + 1;
+    }
 
-int main()
-{
+    // Количество элементов, больших чем num
+    return size - left;
+}
+
+int main() {
     setlocale(LC_ALL, "Russian");
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
-    std::cout << "Введите точку отсчёта : ";
+
+    std::cout << "Введите точку отсчёта: ";
     int Num;
     std::cin >> Num;
+
     int matr[9] = { 14, 16, 19, 32, 32, 32, 56, 69, 72 };
-    std::cout << "Количество элементов в массиве больших, чем " << Num << ":" << CountNums(matr, Num);
+    int size = sizeof(matr) / sizeof(matr[0]); // Определяем размер массива
+
+    std::cout << "Количество элементов в массиве больших, чем " << Num << ": " << CountNums(matr, size, Num);
+
     return 0;
 }
-
