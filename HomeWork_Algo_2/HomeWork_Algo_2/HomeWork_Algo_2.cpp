b701c2ea69b5c5b55a6d0a77080dda268cc9a53a
Alexandr Suhanov <delosk@gmail.com>
2024-09-20 22:58:47 +0300

    initial commit

---
+++ HomeWork_Algo_2/HomeWork_Algo_2/HomeWork_Algo_2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+
+
+
+int fib(int n) {
+    
+    if (n == 1) {
+        return 0;
+    }
+    if (n == 2) {
+        return 1;
+    }
+
+    
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int n;
+    std::cout << "Введите порядковый номер числа Фибоначчи (натуральное число): ";
+    std::cin >> n;
+
+    if (n <= 0) {
+        std::cout << "Пожалуйста, введите натуральное число (порядковый номер)." << std::endl;
+    }
+    else {
+        std::cout << "Число Фибоначчи " << n << ": " << fib(n) << std::endl;
+    }
+
+    return 0;
+}
---
+++ HomeWork_Algo_2_2/HomeWork_Algo_2_2/HomeWork_Algo_2_2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+const int MAX_N = 50; 
+
+int fib(int n, unsigned long memo[]) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    if (memo[n] != -1) {
+        return memo[n];
+    }
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    int n{ -999 };
+
+    do {
+        std::cout << "Введите порядковый номер числа Фибоначчи (натуральное число) не больше " << MAX_N << ": ";
+        std::cin >> n;
+    } while (n <= 0 || n > MAX_N);
+
+        unsigned long memo[MAX_N]; 
+        memo[0] = 0;
+        memo[1] = 1;
+        if (n > 2) {
+            for (int i = 2; i < n; i++) {
+                memo[i] = -1;
+            };
+        };
+        
+        
+        std::cout << "Число Фибоначчи с порядковым номером " << n << ": " << fib(n-1, memo) << std::endl;
+        
+    
+
+    return 0;
+}
